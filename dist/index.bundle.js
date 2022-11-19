57d12ff38e3021af23753e8c0952c81b9d06df92
Evan <eedumas@gmail.com>
2022-11-19 13:38:51 -0800

    Make working javascript bundle

--- dist/index.bundle.js
+++ dist/index.bundle.js
@@ -126,7 +126,17 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ \"./src/style.css\");\n/* harmony import */ var _about__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./about */ \"./src/about.js\");\n\n\n\n(0,_about__WEBPACK_IMPORTED_MODULE_1__[\"default\"])();\n\n\n\n//# sourceURL=webpack://top-restaurant-page/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ \"./src/style.css\");\n/* harmony import */ var _about__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./about */ \"./src/about.js\");\n/* harmony import */ var _nighthawks_jpg__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./nighthawks.jpg */ \"./src/nighthawks.jpg\");\n\n\n\n\n(0,_about__WEBPACK_IMPORTED_MODULE_1__[\"default\"])(); // gotta properly say it's a function yeah.\n\n// lets put a header with the name of the place\n// lets put in an image\n\n// let's call the diner the late night\nconst container = document.querySelector('#container');\n\n\nconst header = document.createElement('div');\nconst headerText = document.createElement('h1');\n\nconst diner = new Image();\ndiner.src = _nighthawks_jpg__WEBPACK_IMPORTED_MODULE_2__;\n\nheaderText.textContent = \"The Late Night\";\n\nconst dinerCaption = document.createElement('figcaption');\ndinerCaption.textContent = \"Nighthawks by Edward Hopper, 1947\"\n\ncontainer.appendChild(header);\n\nheader.appendChild(headerText);\nheader.appendChild(diner);\nheader.appendChild(dinerCaption);\n\n\n\n//# sourceURL=webpack://top-restaurant-page/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/nighthawks.jpg":
+/*!****************************!*\
+  !*** ./src/nighthawks.jpg ***!
+  \****************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"6e187d3868b9ee81cc5e.jpg\";\n\n//# sourceURL=webpack://top-restaurant-page/./src/nighthawks.jpg?");
 
 /***/ })
 
@@ -181,6 +191,18 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _sty
 /******/ 		};
 /******/ 	})();
 /******/ 	
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
 /******/ 	(() => {
 /******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
@@ -195,6 +217,26 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _sty
 /******/ 			}
 /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
 /******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) scriptUrl = scripts[scripts.length - 1].src
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/nonce */
--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 import './style.css';
 import about from './about';
+import nighthawks from './nighthawks.jpg';
 
 about(); // gotta properly say it's a function yeah.
+
+// lets put a header with the name of the place
+// lets put in an image
+
+// let's call the diner the late night
+const container = document.querySelector('#container');
+
+
+const header = document.createElement('div');
+const headerText = document.createElement('h1');
+
+const diner = new Image();
+diner.src = nighthawks;
+
+headerText.textContent = "The Late Night";
+
+const dinerCaption = document.createElement('figcaption');
+dinerCaption.textContent = "Nighthawks by Edward Hopper, 1947"
+
+container.appendChild(header);
+
+header.appendChild(headerText);
+header.appendChild(diner);
+header.appendChild(dinerCaption);
 
